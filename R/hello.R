c7597502de75a13f60992549a37a4d6cbd1e4b4d
AugustArnstad <august.arnstad@askeladden.co>
2024-03-15 12:09:53 +0100

    Poisson & Binomial

--- R/hello.R
+++ R/hello.R
@@ -336,6 +336,159 @@ sample_posterior <- function(model, formula, data, n_samp=1000, additive_param=N
 }
 
 
+sample_posterior_count <- function(model, formula, data, n_samp=1000, additive_param=NULL, param_of_interest=NULL, distribution_var=0) {
+
+  # Make sure its correct with distributional variance
+  # Make the param_of_interest a general input object for all functions. That was a nice way to put it
+  # Scaling not complete
+
+
+  response <- all.vars(formula)[1]
+  scaled_response <- scale(data[, response])
+  scale_const <- attr(scaled_response, 'scaled:scale')
+
+  #fixed <- model$names.fixed[2:length(model$names.fixed)]
+  effects <- extract_effects(formula)
+  fixed <- effects$fixed_effects
+
+
+
+  response <- all.vars(formula)[1]
+
+  variance_marginals_list <- lapply(model$marginals.hyperpar, function(x) inla.tmarginal(function(t) 1/t, x))
+  random <- names(variance_marginals_list)
+  random <- gsub("Precision for the ", "", random)
+  random <- gsub("Precision for ", "", random)
+
+  beta_mat <- matrix(NA, nrow=n_samp, ncol=length(fixed))
+  scaled_beta_mat <- matrix(NA, nrow=n_samp, ncol=length(fixed))
+  importance_mat <- matrix(NA, nrow=n_samp, ncol=length(fixed))
+  scaled_importance_mat <- matrix(NA, nrow=n_samp, ncol=length(fixed))
+  names(importance_mat) <- fixed
+  R2_mat <- matrix(NA, nrow=n_samp, ncol=1)
+  R2_cond_mat <- matrix(NA, nrow=n_samp, ncol=1)
+  var_pred_mat <- matrix(NA, nrow=n_samp, ncol=1)
+  h2_mat <- matrix(NA, nrow=n_samp, ncol=1)
+  random_mat <- matrix(NA, nrow=n_samp, ncol=length(random))
+  scaled_random_mat <- matrix(NA, nrow=n_samp, ncol=length(random))
+
+  if(!is.null(fixed)){
+    SVD <- VariableImportanceINLA::SVD_decomp(data[fixed])
+
+    lambda <- SVD$lambda
+  }else {
+    lambda <- diag(length(fixed))
+  }
+
+  samps_Z <- inla.posterior.sample(model, n = n_samp)
+
+  latent_row_names <- rownames(samps_Z[[1]]$latent)
+
+  output_length=length(samps_Z[[1]]$latent)
+
+  not_na = which(!is.na(data[response]))
+
+  print(random)
+
+  for (i in 1:n_samp){
+    # Extract all sampled values, separate them by covariate/predictor, and assign them to the sampled matrix
+    samples_length <- 0
+    predictor <- paste0("^Predictor:")
+    predictor_names <- grep(predictor, latent_row_names, value = TRUE)
+    predictor_samples <- samps_Z[[i]]$latent[predictor_names, , drop = FALSE]
+    samples_tot <- length(predictor_samples)
+    #print(predictor_samples)
+    #gaussian <- data[[response]][not_na] - predictor_samples[not_na]
+    #var_pred_mat[i] <- var(predictor_samples)
+    #random_mat[i, 1] <- var(gaussian)
+
+
+
+    if (length(random)>0){
+      for (j in 1:length(random)){
+        pattern <- paste0("^", random[j], ":")
+        random_names <- grep(pattern, latent_row_names, value = TRUE)
+        random_samples <- samps_Z[[i]]$latent[random_names, , drop = FALSE]
+        samples_tot <- samples_tot + length(random_samples)
+        random_mat[i, j] <- var(random_samples)
+      }
+    }else{
+      if (i==n_samp){
+        print("No random effects, only resiudal variance")
+      }
+
+    }
+
+    beta <- samps_Z[[i]]$latent[(samples_tot+2):output_length]  #Skip intercept
+    #print(beta)
+    beta_mat[i, ] <- beta
+    #scaled_beta <- beta/scale_const
+    #scaled_beta_mat[i, ] <- scaled_beta
+    importance_mat[i, ] <- lambda^2 %*% beta^2
+    #scaled_importance_mat[i, ] <- lambda^2 %*% scaled_beta^2
+
+
+  }
+
+  rowsum <- rowSums(random_mat) + rowSums(importance_mat) + distribution_var
+  scaled_random_mat <- random_mat/rowsum
+  scaled_beta_mat <- beta_mat/rowsum
+  scaled_importance_mat <- importance_mat/rowsum
+
+  # Do not think these are correct!!!!! They do not contain the guassian observations for example.
+  # Could possibly use the variance of the predictor as the measure
+
+  R2_mat <- rowSums(importance_mat) / (rowSums(importance_mat) + rowSums(random_mat) + distribution_var)
+
+  if (length(random)>1){
+    R2_cond <- (rowSums(importance_mat) + rowSums(random_mat)) / (rowSums(importance_mat) + rowSums(random_mat) + distribution_var)
+  }else if (length(random)==1){
+    R2_cond <- (rowSums(importance_mat) + random_mat ) / (rowSums(importance_mat) + rowSums(random_mat) + distribution_var)
+  }else{
+    R2_cond <- rowSums(importance_mat)  / (rowSums(importance_mat) + rowSums(random_mat) + distribution_var)
+  }
+
+
+  beta_mat <- as.data.frame(beta_mat)
+  names(beta_mat) <- fixed
+  importance_mat <- as.data.frame(importance_mat)
+  names(importance_mat) <- fixed
+  scaled_beta_mat <- as.data.frame(scaled_beta_mat)
+  names(scaled_beta_mat) <- fixed
+  scaled_importance_mat <- as.data.frame(scaled_importance_mat)
+  names(scaled_importance_mat) <- fixed
+
+  random_mat <- as.data.frame(random_mat)
+  names(random_mat) <- random
+  scaled_random_mat <- as.data.frame(scaled_random_mat)
+  names(scaled_random_mat) <- random
+
+  R2_mat <- as.data.frame(R2_mat)
+  names(R2_mat) <- "Marginal R2"
+  R2_cond <- as.data.frame(R2_cond)
+  names(R2_cond) <- "Conditional R2"
+
+  if (!is.null(additive_param)){
+    h2_mat <- random_mat[, additive_param]/(rowSums(importance_mat) + rowSums(random_mat) + distribution_var)
+    h2_mat <- as.data.frame(h2_mat)
+    names(h2_mat) <- paste0("Heritability of: ", additive_param)
+  }
+
+
+  return(list(beta_samples = beta_mat,
+              importance_samples = importance_mat,
+              scaled_beta_samples = scaled_beta_mat,
+              scaled_importance_samples = scaled_importance_mat,
+              random_samples = random_mat,
+              scaled_random_samples = scaled_random_mat,
+              R2_marginal = R2_mat,
+              R2_conditional = R2_cond,
+              var_y = var_pred_mat,
+              heritability = h2_mat))
+}
+
+
+
 #' Plot Samples from Posterior Distributions
 #'
 #' Generates plots for the scaled importance of fixed and random effects,
