3122ef45b8b6748f8d53e3ed8f21985ef6ee3839
AugustArnstad <august.arnstad@askeladden.co>
2024-05-07 13:36:55 +0200

    Fix

--- R/hello.R
+++ R/hello.R
@@ -537,6 +537,7 @@ sample_posterior_count <- function(model, formula, data, n_samp=1000, additive_p
   R2_cond_mat <- matrix(NA, nrow=n_samp, ncol=1)
   var_pred_mat <- matrix(NA, nrow=n_samp, ncol=1)
   h2_mat <- matrix(NA, nrow=n_samp, ncol=1)
+  repeat_mat <- matrix(NA, nrow=n_samp, ncol=1)
   random_mat <- matrix(NA, nrow=n_samp, ncol=length(random))
   scaled_random_mat <- matrix(NA, nrow=n_samp, ncol=length(random))
 
