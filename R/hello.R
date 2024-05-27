e06764b2a796c7570269dda1dbda300c4bf341df
AugustArnstad <august.arnstad@askeladden.co>
2024-05-27 20:15:04 +0200

    extract_importance

--- R/hello.R
+++ R/hello.R
@@ -171,57 +171,43 @@ perform_inla_analysis <- function(data, formula, family, link_func="identity", i
 #'
 #' @param model The fitted model object.
 #' @param data The data used for fitting the model.
-#' @param random_names A vector of names of the random effects.
-#' @param fixed_names A vector of names of the fixed effects.
 #' @param dist_factor Distributional variance factor, used to calculate residual variance. If NULL, the family and link will be checked. If the family is binomial with logit link, the dist_factor will be set to pi^2 / 3. If the family is binomial with probit link, the dist_factor will be set to 1. If the family is poisson, the dist_factor will be calculated as described in Nakagawa et. al - A general and simple method for obtaining R2 from generalized linear mixed-effects models (2013) and Nakagawa et. al - The coefficient of determination R2 and intra-class correlation coefficient from generalized linear mixed-effects models revisited and expanded (2017). If the family does not match any of these, the dist_factor will be set to 0, corresponding to Gaussian responses (LMM)
 #' @return A list containing normalized importance values for random and fixed effects, marginal and conditional R2 values, and expected importances.
 #' @examples
-#' # Assuming `model` is an INLA model object and `data` contains appropriate predictors
-#' importance <- extract_importances(model, data, dist_factor = pi^2 / 3, random_names = "Z1", fixed_names = c("X1", "X2", "X3"))
+#' # Assuming `model` is an INLA model object with log, logit or probit link and `data` contains appropriate predictors
+#' importance <- extract_importances(model, data, dist_factor = pi^2 / 3)
 #' @export
-extract_importances <- function(model, data, random_names, fixed_names, dist_factor=NULL) {
-  # Decompose the fixed effects matrix using SVD
-  SVD <- BayesianVariableImportance::SVD_decomp(data[, fixed_names])
-
-  # Sample from the posterior distribution
-  sample_post <- inla.posterior.sample(model, n = 1)
-
-  # Extract latent effects names
-  latent_names <- rownames(sample_post[[1]]$latent)
-
-  # Initialize a named vector to store variances for each random effect
-  imp_random <- setNames(numeric(length(random_names)), random_names)
-
-  # Iterate through each random effect name and extract corresponding samples
-  for (i in seq_along(random_names)) {
-    pattern_random <- paste0("^", random_names[i], ":")
-    random_indices <- grep(pattern_random, latent_names, value = TRUE)
-    random_samples <- sample_post[[1]]$latent[random_indices, , drop = FALSE]
-
-    # Variance of the current random effect
-    imp_random[i] <- var(random_samples)
-  }
+extract_importances <- function(model, data, dist_factor=NULL) {
 
   # Calculate residual variance
   residual_var <- 0
+
   if ("Precision for the Gaussian observations" %in% rownames(model$summary.hyperpar)) {
     residual_var <- 1 / model$summary.hyperpar["Precision for the Gaussian observations", "mean"]
+    randoms <- summary(model)$hyperpar[, c("mode")]
+    random_effects <- randoms[-1]
+    random_names <- rownames(model$summary.hyperpar)[-1]
+  }else{
+    randoms <- summary(model)$hyperpar[, c("mode")]
+    random_names <- rownames(model$summary.hyperpar)
   }
 
-  # Initialize a named vector to store the fixed effects importances
-  imp_fixed <- setNames(numeric(length(fixed_names)), fixed_names)
-
-  # Extract and calculate importance for each fixed effect
-  fixed_effects <- numeric(length(fixed_names))
-  for (i in seq_along(fixed_names)) {
-    fixed_indices <- grep(paste0("^", fixed_names[i], ":"), latent_names, value = TRUE)
-    fixed_samples <- sample_post[[1]]$latent[fixed_indices, , drop = FALSE]
-
-    # Store the extracted fixed effect sample
-    fixed_effects[i] <- fixed_samples
+  imp_random <- setNames(numeric(length(random_names)), random_names)
+  for (i in seq_along(random_names)) {
+    prec_random <- summary(model)$hyperpar[c(i), c("mode")]
+    imp_random[i] <- 1/prec_random
   }
 
-  # Calculate fixed effect importance using SVD
+  random_names <- gsub("Precision for ", "", random_names)
+
+  fixed_means <- summary(model)$fixed[, c("mode")]
+  fixed_effects <- fixed_means[-1]
+  fixed_names <- names(fixed_effects)
+
+  SVD <- BayesianVariableImportance::SVD_decomp(data[, fixed_names])
+
+  imp_fixed <- setNames(numeric(length(fixed_effects)), fixed_names)
+
   imp_fixed <- (SVD$lambda^2 %*% (fixed_effects^2))
 
   fam <- model$.args$family
@@ -233,26 +219,13 @@ extract_importances <- function(model, data, random_names, fixed_names, dist_fac
       if (link == "probit"){
         dist_factor <- 1
       } else if (link == "logit"){
-
-        #Testing 26.05.24
-        #intercept <- model$summary.fixed["(Intercept)", "mean"]
-        #dist_factor <- stats::plogis(intercept*sqrt(1+((16*sqrt(3))/(15*pi))^2*(sum(imp_random)+sum(imp_fixed)))^-1)
         dist_factor <- pi^2/3
 
-
-        # I think this could be difficult to implement. Ask Steffi.
-
-        #intercept <- samps_Z[[i]]$latent[output_length-length(fixed)]
-        #fixed_contribution <- as.matrix(data[, fixed]) %*% samps_Z[[i]]$latent[(samples_tot+2):output_length]
-
-        #distribution_var <- inv.logit(intercept + fixed_contribution - 0.5*total_latent_var * tanh(((intercept+)*(1+2exp(-0.5*total_latent_var)))/6))
       }
     }else if (fam == "poisson"){
       if (link == "log"){
         intercept <- model$summary.fixed["(Intercept)", "mean"]
-        #Testing 26.05.24
         lambda_pois <- exp(intercept + 0.5*(sum(imp_random) + sum(imp_fixed)))
-        #lambda_pois <- exp(intercept + 0.5*sum(imp_random))
         dist_factor <- log(1 + 1/lambda_pois)
       }else if (link == "root"){
         dist_factor <- 0.25
@@ -265,6 +238,7 @@ extract_importances <- function(model, data, random_names, fixed_names, dist_fac
 
   # Calculate total variance
   total_var <- as.double(dist_factor + sum(imp_random) + sum(imp_fixed) + residual_var)
+
 
   # Calculate marginal and conditional R^2
   r2m <- sum(imp_fixed) / total_var
@@ -281,7 +255,7 @@ extract_importances <- function(model, data, random_names, fixed_names, dist_fac
   imp_random <- imp_random / total_var
   imp_fixed <- imp_fixed / total_var
 
-  rownames(imp_fixed) <- fixed_names
+  #rownames(imp_fixed) <- fixed_names
 
 
   # Return the results as a list with named importances
