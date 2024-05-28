9c77f6ade4c08a44447a2a0296071a535567fb36
AugustArnstad <august.arnstad@askeladden.co>
2024-05-28 11:05:26 +0200

    Data insert

---
+++ R/data.R
@@ -0,0 +1,33 @@
+#' d.morph_no_ringnr data
+#'
+#' A dataset containing morphological measurements of some species, excluding the ring number.
+#'
+#' @format A data frame with 4625 rows and 24 variables:
+#' \describe{
+#'   \item{ID}{int: ID of the individual}
+#'   \item{indnr}{int: Individual number}
+#'   \item{sex}{num: Sex of the individual}
+#'   \item{island_current}{Factor: Current island}
+#'   \item{adultSNPisland}{int: Adult SNP island}
+#'   \item{hatchisland}{int: Hatch island}
+#'   \item{hatchyear}{Factor: Hatch year}
+#'   \item{year}{Factor: Year}
+#'   \item{month}{num: Month}
+#'   \item{age}{num: Age}
+#'   \item{tarsus}{num: Tarsus length}
+#'   \item{wing}{num: Wing length}
+#'   \item{mass}{num: Mass}
+#'   \item{island}{int: Island}
+#'   \item{FGRM}{num: FGRM value}
+#'   \item{id}{int: ID}
+#'   \item{billD}{num: Bill depth}
+#'   \item{inner}{num: Inner measurement}
+#'   \item{outer}{num: Outer measurement}
+#'   \item{other}{num: Other measurement}
+#'   \item{IDC}{int: IDC value}
+#'   \item{IDC2}{int: IDC2 value}
+#'   \item{IDC3}{int: IDC3 value}
+#'   \item{IDC4}{int: IDC4 value}
+#' }
+#' @source Data from sparrows on the Helgeland coast, Norway
+"d.morph_no_ringnr"
